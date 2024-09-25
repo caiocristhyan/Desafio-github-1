13362bd684c88c5ae79230d0d5e62b1fb76547a9
CaioCristhyan <cristhyan.caio@outlook.com>
2024-09-24 22:03:15 -0300

    commit do primeiro desafio github

---
+++ concatena_string.py
@@ -0,0 +1,4 @@
+info1 = input("Digite a primeira informação: ");
+info2 = input("Digite a segunda informação: ");
+
+print(info1 + " " + info2);
---
+++ repeat_txt.py
@@ -0,0 +1,4 @@
+numeroVezes = int(input("Digite o número de vezes que deseja repetir o texto: "));
+texto = input("Digite o texto que deseja repetir: ");
+
+print((texto + " ") * numeroVezes);
